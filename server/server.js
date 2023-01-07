90f97fcee7b924c65f9c9edfe5a51a75c64c65b9
Anatoly <anatoly.makeyev@gmail.com>
2023-01-07 19:07:51 +0200

    added CSRF

--- server/server.js
+++ server/server.js
@@ -2,10 +2,13 @@ import { readdirSync } from 'fs'
 import mongoose from 'mongoose'
 import express from 'express'
 import cors from 'cors'
+import csrf from 'csurf'
+import cookieParser from 'cookie-parser'
 
 require('dotenv').config()
 const morgan = require('morgan')
 const app = express()
+const csrfProtection = csrf({ cookie: true })
 
 // database
 mongoose.connect(process.env.DATABASE).then(() => {
@@ -24,11 +27,18 @@ mongoose.connect(process.env.DATABASE).then(() => {
 // apply middlewares
 app.use(cors())
 app.use(express.json())
+app.use(cookieParser())
 app.use(morgan('dev'))
 
 // routes
 readdirSync('./routes').map((route) => {
     app.use('/api', require(`./routes/${route}`))
+})
+
+// CSRF Protection
+app.use(csrfProtection)
+app.get('/api/csrf-token', (request, response) => {
+    response.json({ csrfToken: request.csrfToken() })
 })
 
 // port
